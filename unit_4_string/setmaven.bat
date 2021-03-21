f6218cb0a31e933437f698cac994eef829d9247c
Daria Garmash <garmashdasha02@gmail.com>
2021-03-21 21:05:14 +0200

    fixed setmaven.bat so that it will create a directory

--- unit_4_string/setmaven.bat
+++ unit_4_string/setmaven.bat
@@ -1,6 +1,6 @@
 @echo off
 set MAVEN_OPTS=-Xmx2G -Dfile.encoding=UTF-8
-
+md apache-maven
 set MAVEN_HOME=%~dp0apache-maven
 set PATH=%MAVEN_HOME%\bin;%PATH%
 echo Setting maven home to: %MAVEN_HOME%
