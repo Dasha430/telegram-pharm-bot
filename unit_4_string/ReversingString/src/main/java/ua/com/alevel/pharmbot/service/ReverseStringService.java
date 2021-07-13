31d6fcae6b2d2b7fc4a63441b157c4ebfaba7143
Dasha430 <74000196+Dasha430@users.noreply.github.com>
2021-07-13 19:22:32 +0300

    Delete unit_4_string directory

--- unit_4_string/ReversingString/src/main/java/ua/com/alevel/pharmbot/service/ReverseStringService.java
+++
@@ -1,9 +0,0 @@
-package ua.com.alevel.service;
-
-public interface ReverseStringService {
-    String reverse(String var1);
-
-    String reverse(String var1, String var2);
-
-    String reverse(String var1, int var2, int var3);
-}
--- unit_4_string/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo -------------Setting maven-------------
-call setmaven.bat
-echo -------------Homework 3-------------
-call mvn clean install
-java -jar ReversingString/target/ReversingString-1.0-SNAPSHOT-jar-with-dependencies.jar
-@pause
